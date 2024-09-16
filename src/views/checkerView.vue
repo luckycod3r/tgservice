4421df237ee21da70e5db63dbfe11b6c8fab4d0e
luckycod3r <leni4kostenko@yandex.ru>
2024-09-16 20:41:41 +0300

    fix ws

--- src/views/checkerView.vue
+++ src/views/checkerView.vue
@@ -79,7 +79,7 @@ export default {
             let task = request.data.task_id;
             if(request.status == 200){
                 this.$store.state.taskID = task;
-                let socket = new WebSocket("https://checker.tg-service.pro/api/task_ws?task_id=" + task);
+                let socket = new WebSocket("wss://checker.tg-service.pro/api/task_ws?task_id=" + task);
                 socket.onmessage = (ev)=>{
                     let json = JSON.parse(ev);
                     if(json.progress >= 99){
